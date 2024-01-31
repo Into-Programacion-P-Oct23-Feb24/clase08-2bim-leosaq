d1f34a0b0eff03ae291f947411f80e968c3c510d
leosaq <leandrosaquisari7@gmail.com>
2024-01-31 15:53:09 -0500

    cambios

--- Ejercicios07/src/manejoarray/ArrayList03.java
+++ Ejercicios07/src/manejoarray/ArrayList03.java
@@ -9,20 +9,14 @@ package manejoarray;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-/**  //q ingrese valores pero antes en una funcion convertir en mayusculas 
+/**
  *
  * @author reroes
  */
-public class ArrayList02 {
+public class ArrayList03 {
 
     public static void main(String[] args) {
-     
-        ArrayList<String> arreglo2 = obtenerDatos();
-        String cadenaFinal = obtenerCadenaFinal(arreglo2);
-        System.out.printf("%s\n", cadenaFinal);
-    }
-
-    public static ArrayList<String> obtenerDatos(){
+        
         ArrayList<String> arreglo2 = new ArrayList<>();
         boolean bandera = true;
         String pais;
@@ -31,34 +25,25 @@ public class ArrayList02 {
         while (bandera) {
             System.out.println("Ingrese un nombre de un país");
             pais = entrada.nextLine();
-            pais= convertirMayuscula(pais);
             arreglo2.add(pais);
-            System.out.println("Desea ingresar salir, ingrese (s)");
+            System.out.println("Desea salir, ingrese (s)");
             opcion = entrada.nextLine();
             if (opcion.equals("s")) {
                 bandera = false;
             }
         }
-        return arreglo2;
         
-    }
-        public static String convertirMayuscula(String a){
-            String dato = a.toUpperCase();
-            return dato;
-        }
-    public static String obtenerCadenaFinal(ArrayList<String> lista){
-        String cadenaFinal = "";
+        System.out.println("Presentar los valor en pantalla");
         
-        for (int i = 0; i < lista.size(); i++) {
-            cadenaFinal = String.format("%s%s\n", cadenaFinal,
-                    lista.get(i));
+        for (int i = 0; i < arreglo2.size(); i++) {
+            System.out.printf("%s\n", arreglo2.get(i));
         }
-        return cadenaFinal;
-    }    
+        
+        
         /*
         ArrayList <String> arreglo2 = new ArrayList<>();
         
         ArrayList <Boolean> arreglo4 = new ArrayList<>();
          */
-    
+    }
 }
